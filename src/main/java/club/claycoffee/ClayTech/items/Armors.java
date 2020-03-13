86cc734566e70bec48e3dfb57ffb9ef58fd64826
ClayCoffee <1020757140@qq.com>
2020-03-13 11:55:40 +0800

    [F] nightly-20200313

--- src/main/java/club/claycoffee/ClayTech/items/Armors.java
+++ src/main/java/club/claycoffee/ClayTech/items/Armors.java
@@ -1,11 +1,10 @@
 package club.claycoffee.ClayTech.items;
 
-import org.bukkit.Material;
 import org.bukkit.NamespacedKey;
-import org.bukkit.inventory.ItemStack;
 
 import club.claycoffee.ClayTech.ClayTech;
 import club.claycoffee.ClayTech.Defines;
+import club.claycoffee.ClayTech.Recipes;
 import club.claycoffee.ClayTech.TRecipe;
 import club.claycoffee.ClayTech.utils.Lang;
 import club.claycoffee.ClayTech.utils.Slimefunutils;
@@ -14,13 +13,9 @@ import me.mrCookieSlime.Slimefun.Objects.SlimefunItem.SlimefunItem;
 
 public class Armors {
 	public Armors() {
-		ItemStack[] recipea = { Defines.ADVANCED_SLOWNESS_CORE, Defines.ADVANCED_SLOWNESS_CORE,
-				Defines.ADVANCED_SLOWNESS_CORE, Defines.ADVANCED_SLOWNESS_CORE, new ItemStack(Material.IRON_BOOTS),
-				Defines.ADVANCED_SLOWNESS_CORE, Defines.ADVANCED_SLOWNESS_CORE, Defines.ADVANCED_SLOWNESS_CORE,
-				Defines.ADVANCED_SLOWNESS_CORE };
 
 		Slimefunutils.registerItem(Defines.C_ARMORS, "ANTI_SLOWNESS_BOOTS", Defines.ANTI_SLOWNESS_BOOTS, "notresearch",
-				10, TRecipe.CLAY_CRAFTING_TABLE, recipea, false);
+				10, TRecipe.CLAY_CRAFTING_TABLE, Recipes.ANTI_SLOWNESS_BOOTS, false);
 
 		Research basic = new Research(new NamespacedKey(ClayTech.plugin, "CLAYTECH_ARMORS"), 9908,
 				Lang.readResearchesText("CLAYTECH_ARMORS_I"), 50);
