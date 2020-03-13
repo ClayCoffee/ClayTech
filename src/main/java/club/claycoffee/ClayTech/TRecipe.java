0ac5ff7947a0b19acdaceae6f588752e567fc2b1
ClayCoffee <1020757140@qq.com>
2020-03-13 11:37:02 +0800

    [F] nightly-20200313

--- src/main/java/club/claycoffee/ClayTech/TRecipe.java
+++ src/main/java/club/claycoffee/ClayTech/TRecipe.java
@@ -1,21 +1,22 @@
 package club.claycoffee.ClayTech;
 
+import org.bukkit.NamespacedKey;
+
 import club.claycoffee.ClayTech.utils.Lang;
 import me.mrCookieSlime.Slimefun.Lists.RecipeType;
 import me.mrCookieSlime.Slimefun.api.SlimefunItemStack;
 
-@SuppressWarnings("deprecation")
 public class TRecipe {
 	public static final RecipeType CLAY_CRAFTING_TABLE = new RecipeType(
-			new SlimefunItemStack("CLAY_CRAFTING_TABLE", Defines.CLAY_CRAFTING_TABLE), "",
+			new NamespacedKey(ClayTech.plugin,"CLAY_CRAFTIONG_TABLE"),new SlimefunItemStack("CLAY_CRAFTING_TABLE", Defines.CLAY_CRAFTING_TABLE), "",
 			Lang.readMachineRecipesText("CLAY_FUSION_MACHINE"));
 	public static final RecipeType CLAY_FOOD_CAULDRON = new RecipeType(
-			new SlimefunItemStack("CLAY_FOOD_CAULDRON", Defines.CLAY_FOOD_CAULDRON), "",
+			new NamespacedKey(ClayTech.plugin,"CLAY_FOOD_CAULDRON"),new SlimefunItemStack("CLAY_FOOD_CAULDRON", Defines.CLAY_FOOD_CAULDRON), "",
 			Lang.readMachineRecipesText("CLAY_ELETRIC_CAULDRON"));
 	public static final RecipeType CLAY_FOOD_CHALKING_MACHINE = new RecipeType(
-			new SlimefunItemStack("CLAY_FOOD_CHALKING_MACHINE", Defines.CLAY_FOOD_CHALKING_MACHINE), "",
+			new NamespacedKey(ClayTech.plugin,"CLAY_FOOD_CHALKING_MACHINE"),new SlimefunItemStack("CLAY_FOOD_CHALKING_MACHINE", Defines.CLAY_FOOD_CHALKING_MACHINE), "",
 			Lang.readMachineRecipesText("CLAY_FOOD_CHALKING_MACHINE"));
 	public static final RecipeType CLAY_ELEMENT_EXTRACTER = new RecipeType(
-			new SlimefunItemStack("CLAY_ELEMENT_EXTRACTER", Defines.CLAY_ELEMENT_EXTRACTER), "",
+			new NamespacedKey(ClayTech.plugin,"CLAY_ELEMENT_EXTRACTER"),new SlimefunItemStack("CLAY_ELEMENT_EXTRACTER", Defines.CLAY_ELEMENT_EXTRACTER), "",
 			Lang.readMachineRecipesText("CLAY_ELEMENT_EXTRACTER"));
 }
