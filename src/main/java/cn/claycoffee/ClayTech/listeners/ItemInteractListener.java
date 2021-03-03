63b4ef1c1e462f09e40fdd2eb2b760124e78b50a
ClayCoffee <1020757140@qq.com>
2021-03-03 13:44:26 +0800

    [F] Fixed event priority (closed #108)

--- src/main/java/cn/claycoffee/ClayTech/listeners/ItemInteractListener.java
+++ src/main/java/cn/claycoffee/ClayTech/listeners/ItemInteractListener.java
@@ -9,18 +9,17 @@ import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 
 public class ItemInteractListener implements Listener {
-    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
     public void BlockPlaceEvent(BlockPlaceEvent e) {
         if (Utils.ExitsInList(Lang.readGeneralText("CantPlaceLore"), Utils.getLore(e.getItemInHand()))
                 && !e.isCancelled()) {
             e.setCancelled(true);
             e.getPlayer().sendMessage(Lang.readGeneralText("CantPlace"));
         }
-
     }
 
     @SuppressWarnings("deprecation")
-    @EventHandler(priority = EventPriority.HIGHEST, ignoreCancelled = true)
+    @EventHandler(priority = EventPriority.LOWEST, ignoreCancelled = true)
     public void PlayerInteractEvent(PlayerInteractEvent e) {
         if (e.hasItem()) {
             if (e.getItem().hasItemMeta() && !e.isCancelled()) {
